50bf1039093dbb880b21b9b6620b3541a2600f25
Janus <Janus.Haung@hualiteq.com>
2025-04-29 19:49:02 +0800

    Initial Commit

---
+++ MyPinkFurnitureStore/MyPinkFurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPinkFurnitureStoreApp.swift
+//  MyPinkFurnitureStore
+//
+//  Created by Hualiteq International on 2025/4/29.
+//
+
+import SwiftUI
+
+@main
+struct MyPinkFurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
